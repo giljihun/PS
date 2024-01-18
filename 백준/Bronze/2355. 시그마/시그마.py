03fdcdbe419dda0d7a92f5376f790f95c4abda2e
GIL <accc45@outlook.com>
2024-01-18 16:12:23 +0900

    [Bronze II] Title: 시그마, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2355. 시그마/시그마.py
+++ 백준/Bronze/2355. 시그마/시그마.py
@@ -1,8 +1,8 @@
-import sys
-input = sys.stdin.readline
+# 1~100까지는 5050. 101/2*(100)
 
-#1~100까지는 5050. 101/2*(100)
 A, B = map(int, input().split())
+
 if A > B:
     A, B = B, A
-print((B-A+1)*(A+B)//2)
+
+print(int(((A+B)/2) * (B-A+1)))
