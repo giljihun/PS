6d2c06ee26328006cecdb5d4912a00fa6e01bd9f
GIL <accc45@outlook.com>
2024-01-24 21:07:40 +0900

    [Silver V] Title: 요세푸스 문제 0, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.py
@@ -0,0 +1,14 @@
+N, K = map(int, input().split())
+
+index = 0
+queue = [num for num in range(1, N+1)]
+answer = []
+
+while queue:
+
+    index += K - 1
+    if index >= len(queue):
+        index %= len(queue)
+    answer.append(str(queue.pop(index)))
+
+print("<", ', '.join(answer), ">", sep="")
