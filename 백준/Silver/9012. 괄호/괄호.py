8bc1fab88304587fcc19c6ded96f19a98b8b3cd7
GIL <accc45@outlook.com>
2024-01-31 17:04:11 +0900

    [Silver IV] Title: 괄호, Time: 48 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/9012. 괄호/괄호.py
@@ -0,0 +1,27 @@
+import sys
+
+T = int(sys.stdin.readline())
+answer = []
+
+for _ in range(T):
+    stack = []
+    s = input()
+    VPS = True
+
+    for ch in s:
+        if ch == '(':
+            stack.append('(')
+        if ch == ')':
+            if stack:
+                stack.pop()
+            elif not stack:
+                VPS = False
+                break
+
+    if not stack and VPS:
+        answer.append("YES")
+    elif stack or not VPS:
+        answer.append("NO")
+
+for ans in answer:
+   print(ans)
