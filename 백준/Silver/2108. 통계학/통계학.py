fcce238903d414d85a515834fd2d745b886f18a7
GIL <accc45@outlook.com>
2024-02-29 14:48:00 +0900

    [Silver III] Title: 통계학, Time: 496 ms, Memory: 52504 KB -BaekjoonHub

--- 백준/Silver/2108. 통계학/통계학.py
+++ 백준/Silver/2108. 통계학/통계학.py
@@ -1,35 +1,39 @@
 import sys
-input=sys.stdin.readline
 
-n=int(input())
-arr=[]
+N = int(sys.stdin.readline())
+nums = []
 
-for i in range(n):
-    arr.append(int(input()))
+for _ in range(N):
+    nums.append(int(sys.stdin.readline()))
 
-arr.sort()#중앙값을 구하기 위해 정렬
+# 정렬
+nums.sort()
 
-print(round(sum(arr)/len(arr)))#1) 산술평균
-print(arr[len(arr)//2])#2) 중앙값
+# 산술평균
+print((round(sum(nums) / N)))
 
-#최빈값
-dic=dict()
-for i in arr:#빈도수 구하기
-    if i in dic:
-        dic[i]+=1
+# 중앙값
+print((nums[len(nums)//2]))
+
+# 최빈값
+many = {}
+for num in nums:
+    if num in many:
+        many[num] += 1
     else:
-        dic[i]=1
-        
-mx=max(dic.values())#빈도수 중 최대값 구하기
-mx_dic=[]#최빈값 숫자를 저장할 배열
+        many[num] = 1
 
-for i in dic:#빈도수 딕셔너리에서
-    if mx==dic[i]:#최빈값의 key저장
-        mx_dic.append(i)
+maxy = max(many.values())
+maxys = []
 
-if len(mx_dic)>1:#최빈값이 여러개라면
-    print(mx_dic[1])#두번째로 작은 값  3)최빈값
-else:#하나라면
-    print(mx_dic[0])#해당 값 출력  3)최빈값
-    
-print(max(arr)-min(arr))#4) 범위
+for i in many:
+    if maxy == many[i]:
+        maxys.append(i)
+
+if len(maxys) > 1:
+    print(maxys[1])
+else:
+    print(maxys[0])
+
+# 범위
+print(max(nums) - min(nums))
