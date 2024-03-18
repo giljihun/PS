dec453d79382943c9f337a6152505cf8cd2d40f9
GIL <accc45@outlook.com>
2024-03-18 14:46:32 +0900

    [Silver V] Title: 색종이, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/2563. 색종이/색종이.py
@@ -0,0 +1,18 @@
+import sys
+
+input = sys.stdin.readline
+
+N = int(input())
+arr = [[0] * 101 for _ in range(101)]
+
+for _ in range(N):
+    x, y = map(int ,input().split())
+    for i in range(x, x+10):
+        for j in range(y, y+10):
+            arr[i][j] = 1
+
+result = 0
+for i in arr:
+    result += sum(i)
+
+print(result)
