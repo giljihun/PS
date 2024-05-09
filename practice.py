de7a840eac8c3fd66302169adb7e0bdb37182122
giljihun <accc45@outlook.com>
2024-05-09 19:58:52 +0900

    240509

--- practice.py
+++ practice.py
@@ -1,2 +1,12 @@
-print((2**(int(input()))+1)**2)
+N, K = map(int ,input().split())
 
+nums = []
+
+for i in range(1, N+1):
+    if N % i == 0:
+        nums.append(i)
+
+if len(nums) < K:
+    print("0")
+else:
+    print(nums[K-1])
