d89af251bd4d4f4a524c3d1fd51223deb325bce9
GIL <accc45@outlook.com>
2024-05-09 20:22:10 +0900

    [Bronze III] Title: 네 번째 점, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/3009. 네 번째 점/네 번째 점.py
@@ -0,0 +1,31 @@
+from itertools import count
+
+
+x = []
+y = []
+target_x = 0
+target_y = 0
+
+for _ in range(3):
+    a, b = map(int, input().split())
+    x.append(a)
+    y.append(b)
+
+if x[0] in x[1:]:
+    if x[0] == x[1]:
+        target_x = x[2]
+    else:
+        target_x = x[1]
+
+else:
+    target_x = x[0]
+
+if y[0] in y[1:]:
+    if y[0] == y[1]:
+        target_y = y[2]
+    else:
+        target_y = y[1]
+else:
+    target_y = y[0]
+
+print(target_x, target_y)
